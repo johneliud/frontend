22fe254581957bd007d33b43ab9221722e741e18
John <johneliud4@gmail.com>
2026-02-20 16:46:30 +0300

    feat: add auth modal components and service to root app component. This update imports SigninComponent, SignupComponent, and CommonModule, and injects AuthModalService to enable conditional rendering of authentication modals directly from the root application template.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,25 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header';
 import { NotificationComponent } from './components/notification/notification';
+import { SigninComponent } from './components/signin/signin';
+import { SignupComponent } from './components/signup/signup';
+import { AuthModalService } from './services/auth-modal.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, NotificationComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    NotificationComponent,
+    SigninComponent,
+    SignupComponent,
+    CommonModule,
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('frontend');
+  constructor(public modalService: AuthModalService) {}
 }
